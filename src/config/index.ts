df701d29d7c7d5573b269cd6dccb7d605258aaed
dipujafar <dipujafarmr@gmail.com>
2026-03-28 17:56:16 +0600

    fiexd .env issue

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const envConfig = {
-  serverBaseApi: "http://206.162.244.154:7128/api/v1" ,
+  serverBaseApi: process.env.SERVER_BASE_API ,
   clientBaseApi: process.env.NEXT_PUBLIC_BASE_API,
   hasSSL: process.env.NEXT_PUBLIC_HAS_SSL == "true" ? true : false,
 };
