c79e4470a2fa686f8390ec352e620a6530b1c641
dipujafar <dipujafarmr@gmail.com>
2026-08-18 12:17:12 +0600

    changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const envConfig = {
-  serverBaseApi: "http://206.162.244.154:7128/api/v1" ,
+  serverBaseApi: "http://72.244.153.29:5000/api/v1" ,
   clientBaseApi: process.env.NEXT_PUBLIC_BASE_API,
   hasSSL: process.env.NEXT_PUBLIC_HAS_SSL == "true" ? true : false,
 };
